e504a4536f3b4f1274654a73948e0b4bbf69c1d0
Kenny Moens <kenny.moens@cipal.be>
2017-03-14 15:12:29 +0100

    rework and simplification

--- org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/CSVTags.java
+++ org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/CSVTags.java
@@ -1,8 +1,10 @@
 package org.eclipse.birt.report.engine.emitter.csv;
 
 public class CSVTags {
+    public static final String SEMICOLON = ";";
+	public static final String COMMA = ",";
 
-	public static final String TAG_COMMA = "," ;
-	public static final String TAG_CR = "\n" ;
-	
+	public static final String QUOTE = "\"";
+
+	public static final String NEWLINE = "\r\n";
 }
--- org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/CSVWriter.java
+++ org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/CSVWriter.java
@@ -1,53 +1,112 @@
 package org.eclipse.birt.report.engine.emitter.csv;
 
-
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.eclipse.birt.report.engine.emitter.XMLWriter;
+/**
+ * Writer for CSV files.
+ *
+ * @author Kenny Moens <kenny.moens@cipalschaubroeck.be>
+ * @since 1.01.2.00
+ */
+public class CSVWriter {
+    private static Logger logger = Logger.getLogger(CSVWriter.class.getName());
 
-public class CSVWriter extends XMLWriter{
-	
-	public CSVWriter(){
-		bImplicitCloseTag = false;
-		bPairedFlag = false;
-	}
-	
-	
-	protected static Logger logger = Logger.getLogger( CSVWriter.class.getName( ) );
+    private final String quote;
+    private final String escapeQuote;
+    private final String sep;
 
-	public void open(OutputStream out, String string) {		
-		super.open(out,string);
-	}
+    private final SimpleDateFormat dateFormat;
+    private final DecimalFormat decimalFormat;
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.birt.report.engine.emitter.XMLWriter#startWriter()
-	 * overridden in CSVWriter to avoid writing XML tagging in beginning of file
-	 */
-	public void startWriter() {
-		// do nothing here. 		
-	}
+    public CSVWriter() {
+        this(CSVTags.QUOTE, CSVTags.SEMICOLON);
+    }
 
-	public void endWriter() {
-		// do nothing flushing will happen in close
-	}
+    public CSVWriter(final String quote, final String sep) {
+        this.quote = quote;
+        this.sep = sep;
 
-	public void close() {		
-		super.close();
-	}
+        escapeQuote = quote + quote;
 
-	public void text(String textValue, String delimiter, String replaceDelimiterInsideTextWith) {
-		if ( textValue == null || textValue.length( ) == 0 )
-		{
-			return;
-		}		
-		// Replacing delimiter in Cell Data with user defined character for CSVWriter
-		textValue = textValue.replace(delimiter,replaceDelimiterInsideTextWith);		
-		print( textValue );		
-	}
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-	public void closeTag( String tagName )
-	{
-		print(tagName);			
-	}
+        DecimalFormatSymbols s = new DecimalFormatSymbols();
+        s.setDecimalSeparator(',');
+
+        decimalFormat = new DecimalFormat("#.##");
+    }
+
+    private Writer writer;
+
+    public void open(final OutputStream outputStream, final String encoding) {
+        assert (encoding != null);
+        assert (outputStream != null);
+
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(outputStream, encoding));
+        } catch (UnsupportedEncodingException e) {
+            logger.log(Level.SEVERE,"the character encoding {0} unsupported !", encoding); //$NON-NLS-1$
+        }
+    }
+
+    public void close() {
+        flush();
+        try {
+            writer.close();
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, ex.getMessage());
+        }
+    }
+
+    public void flush() {
+        try {
+            writer.flush();
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, ex.getMessage());
+        }
+    }
+
+    public void writeField(final Object value) {
+        if (value == null) {
+        } else if (value instanceof Byte || value instanceof Integer || value instanceof Long || value instanceof BigDecimal) {
+            write(value.toString());
+        } else if (value instanceof Number) {
+            write(decimalFormat.format(value));
+        } else if (value instanceof Date) {
+            write(dateFormat.format((Date) value));
+        } else if (value instanceof Calendar) {
+            write(dateFormat.format(((Calendar) value).getTime()));
+        } else {
+            write(quote + value.toString().replace(quote, escapeQuote) + quote);
+        }
+    }
+
+    public void writeSeperator() {
+        write(sep);
+    }
+
+    public void writeEndRow() {
+        write(CSVTags.NEWLINE);
+    }
+
+    private void write(final String text) {
+        try {
+            writer.write(text);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to write CSV file contenxt: " + e.getMessage(), e);
+        }
+    }
 }
